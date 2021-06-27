599f298bb61b55c871b76f5cffa01ced7aed3ef7
pbgodsoo <84000206+pbgodsoo@users.noreply.github.com>
2021-06-27 14:34:19 +0900

    Create 1152.c

---
+++ 1152.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    String s;
+    int i;
+    while(1){
+        if(scanf("%s", &s))
+            c++;
+        else
+            break;
+    }
+    return 0;
+}

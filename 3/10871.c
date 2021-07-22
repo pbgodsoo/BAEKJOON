65cdc11b6b341b62461241a754282137c57a2a99
BeomSoo Park <linoleca@naver.com>
2021-07-22 23:59:38 +0900

    Create 10871.c

---
+++ 3/10871.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x, t;
+    scanf("%d %d", &n, &x);
+    for(int i = 0; i<n; i++)
+    {
+        scanf("%d", &t);
+        if(t<x)
+            printf("%d ", t);
+    }
+}

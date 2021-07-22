934da96cf20475658aaf393195e3b678a53a0d2f
BeomSoo Park <linoleca@naver.com>
2021-07-22 23:58:32 +0900

    Create 11022.c

---
+++ 3/11022.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, b;
+    int j = 0;
+    scanf("%d", &n);
+    for(int i = 0; i<n; i++)
+    {
+        j++;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", j, a, b, a+b);
+    }
+}
